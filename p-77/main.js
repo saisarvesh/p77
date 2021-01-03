16df35e85936cbc39f573bfcc3e9dfd9ddb8d265
saisarvesh <72025326+saisarvesh@users.noreply.github.com>
2021-01-03 12:50:47 +0530

    Add files via upload

---
+++ p-77/main.js
@@ -0,0 +1,17 @@
+console.log("my first console project");
+var first_name = "venkata";
+var last_name = "saisarvesh";
+var full_nme = first_name.concat(last_name);
+console.log(full_nme);
+var a = 10;
+var b = 5;
+var add = a + b;
+console.log(add);
+
+var sub = a - b;
+console.log(sub)
+
+var multiply = a * b;
+console.log(multiply);
+var divide = a / b;
+console.log(divide);
